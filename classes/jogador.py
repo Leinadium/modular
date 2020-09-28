9bfecded2c9c56c3471fa1274866b491e96a444c
Leinadium <daniel.sch.guima@gmail.com>
2020-09-28 11:17:01 -0300

    Criada pasta classes

--- classes/jogador.py
+++ classes/jogador.py
@@ -3,20 +3,26 @@
 from unittest.mock import Mock
 
 
-rodar_dado = Mock(return_value=6)
+gera_numero = Mock(return_value=6)
 
 
 class Jogador:
     dado_atual = None
     vitorioso = False
+    peoes_vivos = 4
+    peoes_finalizados = 0
 
     def __init__(self, cor, nome):
         self.nome = nome
         self.cor = cor
         # self.peoes = [Peao(cor) for i in range(4)]
 
-    def gera_numero(self):
-        self.dado_atual = rodar_dado()
+    def rodar_dado(self):
+        self.dado_atual = gera_numero()
         if 1 <= self.dado_atual <= 6:
             return 0
+        return 1
+
+    def checa_vitoria(self):
+        return self.peoes_finalizados == 4
 
