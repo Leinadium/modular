0ec97280b617f490dcc88f0c4ab141b41543f786
Leinadium <daniel.sch.guima@gmail.com>
2020-10-05 11:31:26 -0300

    funcao jogar_dado

---
+++ entidades/dado.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Trabalho Ludo - Programação Modular 2020.2
+Grupo 1:
+    Daniel Guimarães
+    Gabriel Galvão
+    Guilherme Bizzo
+"""
+
+from random import randint
+
+
+def jogar_dado():
+    return randint(1, 6)
