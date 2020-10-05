3552147cb16f1f76ed0997cb9f4d238b9a4dfca9
Leinadium <daniel.sch.guima@gmail.com>
2020-10-05 13:16:03 -0300

    Recomeçando

---
+++ partida.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Trabalho Ludo - Programação Modular 2020.2
+Grupo 1:
+    Daniel Guimarães
+    Gabriel Galvão
+    Guilherme Bizzo
+"""
+import entidades.jogador
+import entidades.peao
+import entidades.tabuleiro
+import entidades.dado
+
+from unittest.mock import Mock
+
