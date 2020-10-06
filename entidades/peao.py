2747a196900581e0a3b83f862f1111d606342c39
Leinadium <daniel.sch.guima@gmail.com>
2020-10-05 22:02:26 -0300

    refazendo funcoes acesso peao

--- entidades/peao.py
+++ entidades/peao.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Módulo para operacoes com os peoes.
-Cada peao possui um id, uma cor e uma posicao (pos).
+Peao:
+    id: inteiro
+    cor: string
 """
 
 peoes = []
@@ -16,22 +18,19 @@ def limpar_peoes():
     return 0
 
 
-def criar_peao(cor, pos):
+def criar_peao(cor):
     """Cria um peao. Retorna seu id."""
     global id_peao_atual
     peao = dict()
 
     peao['id'] = id_peao_atual
     id_peao_atual += 1
-
-    peao['pos'] = pos
     peao['cor'] = cor
-    peao['posicao_inicial'] = pos
-    peao['deslocamento'] = 0
     peoes.append(peao)
     return peao['id']
 
 
+'''
 def acessar_peoes(pos=-1, cor='', id_peao=-1):
     """Retorna uma lista dos id dos peoes daquela cor ou na posicao."""
     # copia para nao quebrar o encapsulamento
@@ -63,3 +62,4 @@ def reiniciar_peao(id_peao):
             p['deslocamento'] = 0
             return 0
     return 0
+'''
